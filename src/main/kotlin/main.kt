5d3151e89c88968fe82d5cb58af3e99c59b7af19
AlexeYeS <m0use@mail.ru>
2022-08-22 00:39:42 +0300

    add fun master visa vk inlimit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,32 @@
 fun main() {
-    val typeCard = 1
+    val typeCard = 3 // Vk Pay
+    val totalMonthTransfer = 0 // коп
+    val transferCash = 12000 // коп
+
+    val transferFees = when (typeCard) {
+        1 -> masterMaestro(transferCash, totalMonthTransfer)
+        2 -> visaMir(transferCash, totalMonthTransfer)
+        3 -> vkPay(transferCash, totalMonthTransfer)
+        else -> println("Transfer ERROR")
+    }
+    if inLimit() {
+        println("Обьем перевода: $transferCash коп")
+        println("Комиссия: $transferFees коп")
+    } else println("Превышен лимит перевода")
+}
+
+fun masterMaestro(transferCash: Int, totalMonthTransfer: Int) {
+
+}
+
+fun visaMir(transferCash: Int, totalMonthTransfer: Int) {
+
+}
+
+fun vkPay(transferCash: Int, totalMonthTransfer: Int) {
+
+}
+
+fun inLimit(function: () -> Unit) {
 
 }
